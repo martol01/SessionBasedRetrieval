c4429a4e2102eafb7bf81d10cf95d3d02d2c36c5
Dominik Krejcik <dominik.krejcik@gmail.com>
2015-02-06 12:39:25 +0000

    Change indexing script

--- src/main/scripts/indexer.sh
+++ src/main/scripts/indexer.sh
@@ -1,4 +1,4 @@
-ARCHIVE_NAME="archive.warc"
+gARCHIVE_NAME="archive.warc"
 OUTPUT_FOLDER="/Users/ralucamelon/Downloads/indri-5.0/outputIndex"
 
 echo "Param file: $1"
@@ -7,7 +7,7 @@ FILES="$1/*.warc.gz"
 for file in $FILES
 do
 	echo "Unzipping warc.gz file: $file"
-	# extract parameter file in 
+	# extract parameter file in
 	gunzip -c $file > $ARCHIVE_NAME
 	echo "<parameters> \
 	     <memory>400m</memory> \
@@ -16,12 +16,17 @@ do
 	       <name>krovetz</name> \
 	     </stemmer> \
 	     <corpus> \
-	     <path>$ARCHIVE_NAME</path> \       
+	     <path>$ARCHIVE_NAME</path> \
 	     <class>warc</class> \
 	     </corpus> \
 	     <field><name>title</name></field> \
-	     <field><name>date</name><numeric>true</numeric><parserName>DateFieldAnnotator</parserName></field> \
-	</parameters>" > param_file.xml
+	     <field><name>heading</name></field> \
+	     <field><name>body</name></field> \
+	     <metadata> \
+  				<forward>url</forward> \
+  				<backward>url</backward> \
+			 </metadata> \
+			</parameters>" > param_file.xml
 
 	# Run the indexer on the newly unzipped warc file.
 	echo "Indexing documents from warc file"
