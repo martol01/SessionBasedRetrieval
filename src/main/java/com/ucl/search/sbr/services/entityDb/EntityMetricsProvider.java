cc7852423cba401e4b227f4d79b28a60736d2aab
Gabriel Bila <gabriel@mielunegruDebian.mielunegru.debian.com>
2015-03-14 15:56:08 +0000

    Wrote interface for Clueweb entity metrics provider

---
+++ src/main/java/com/ucl/search/sbr/services/entityDb/EntityMetricsProvider.java
@@ -0,0 +1,44 @@
+package com.ucl.search.sbr.services.entityDb;
+
+/**
+ * Wrapper for returning entity metrics for Clueweb documents.
+ *
+ * Created by gabriel on 14/03/15.
+ */
+public interface EntityMetricsProvider {
+
+    /**
+     * @param id the entity's Freebase id
+     * @return the number of times the entity occurs in the corpus
+     */
+    public long getEntityCorpusCount(String id);
+
+    /**
+     * Get the number of (non-unique) entities in the document.
+     * Multiple occurrences of same entity are counted as well.
+     */
+    public long getCorpusLength();
+
+    /**
+     * Get the Freebase text representation of the entity with specified id
+     * @param entityId Freebase identifier of entity
+     * @return the text representation of the entity
+     */
+    public String getEntityText(String entityId);
+
+    /**
+     * Number of times a Clueweb document contains a Freebase entity
+     * @param entityId Freebase id of entity
+     * @param documentId Document id in the Clueweb collection
+     * @return how many times the document contains the entity
+     */
+    public long getEntityDocumentCount(String entityId, String documentId);
+
+    /**
+     * Get the number of entities in the Clueweb document. Counts entity
+     * occurrences, meaning that duplicates are also counted.
+     * @param documentId Clueweb id of document
+     * @return number of entity occurrences in the document
+     */
+    public long getDocumentLength(String documentId);
+}
